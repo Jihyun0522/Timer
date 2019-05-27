8460f6a3eaf2b922c6d48609c122b5dad64588e5
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-05-27 09:03:23 +0900

    #2 타이머 동작 코드 구현

--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -13,9 +13,70 @@ namespace Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            ProcessTimer();
+
+        }
+
+        private void ProcessTimer()
+        {
+            if (IntCheck())
+            {
+                this.CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.txtNum.ReadOnly = true;
+                this.Timer.Enabled = true;
+            }
+            else
+            {
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+        }
+
+        private bool IntCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("숫자를 입력하세요~", "알림",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                MessageBox.Show("펑", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+            else
+            {
+                this.CountOrgNum = this.CountOrgNum - 1;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+            }
+         }
+
+        private void TxtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                ProcessTimer();
+            }
         }
     }
 }
